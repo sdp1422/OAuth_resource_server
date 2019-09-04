3eb5872ff2c8c58828083cdf7dfe58c7febd1411
knut-sdpark <sdp1422@naver.com>
2019-09-04 22:22:53 +0900

    first commit

---
+++ src/main/java/com/sdpark/example/remoteresourceserver/RemoteResourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.sdpark.example.remoteresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteResourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteResourceserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdpark/example/remoteresourceserver/RemoteResourceserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdpark.example.remoteresourceserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RemoteResourceserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
